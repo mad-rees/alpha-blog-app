f9e996101cd7debffa087568fd2286bea425ae2b
Christopher Rees <christopherrees@me.com>
2021-06-14 17:35:58 -0300

    Add index route, controller action and html template.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+  
+  
+  #Index actions
+  def index
+    @articles = Article.all
+  end
+  #Show action
   def show
     @article = Article.find(params[:id])
   end
