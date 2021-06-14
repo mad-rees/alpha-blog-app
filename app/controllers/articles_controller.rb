df74e365dec0cfb1e2edcc74ec56381478d4043d
Christopher Rees <christopherrees@me.com>
2021-06-14 17:55:18 -0300

    Add ‘new’ form and create functionality to save to the database.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def new
+    
+  end
+  
+  def create
+    render plain: params[:article]
+    
+  end
 end
