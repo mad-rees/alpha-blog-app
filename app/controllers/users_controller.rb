aec04ba995f263268a24758d9d52843cba83c850
Christopher Rees <christopherrees@me.com>
2021-06-21 22:48:17 +0000

    Refactor users controller code.  Edit navbar to reflect users login status.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
   
   def show 
-    @user = User.find(params[:id])
     @articles = @user.articles
   end
   
@@ -14,12 +15,12 @@ class UsersController < ApplicationController
   end 
   
   def edit 
-    @user = User.find(params[:id])
+
     
   end
   
   def update 
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:notice] = "Your account was updated successfully."
       redirect_to @user
@@ -31,6 +32,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if@user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Congrats!  #{@user.username}, you have successfully signed up for the blog."
       redirect_to @user
     else
@@ -42,4 +44,8 @@ private
   def user_params 
     params.require(:user).permit(:username, :email, :password)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end 
 end
