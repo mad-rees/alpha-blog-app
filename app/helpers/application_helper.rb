6960655d4a51e5a32e77b746682a38d0e074434f
Christopher Rees <christopherrees@me.com>
2021-06-21 23:24:26 +0000

    Add profile actions to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,5 @@ include Pagy::Frontend
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
-  
-
-  
 
 end
